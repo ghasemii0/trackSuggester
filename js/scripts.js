f56bc48ffdb954cd70646db49c14c63d4658475d
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-03-29 11:07:56 -0700

    make the first suggestion

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,17 @@ $(document).ready(function(){
     var age = parseInt($("#age").val());
     var companySize = $("#bigOrSmall").val();
     var whichEnd = $("#frontEnd-backEnd").val();
-    var usaOrWorldwide = $("input:radio[name=fullTime-PartTime]:checked").val();
+    var fullTimeOrPartTime = $("input:radio[name=fullTime-PartTime]:checked").val();
     var serverSideOrApplictions = $("input:radio[name=which-side]:checked").val();
-    console.log(age);
+    console.log(typeof age);
     console.log(companySize);
     console.log(whichEnd);
-    console.log(usaOrWorldwide);
+    console.log(fullTimeOrPartTime);
     console.log(serverSideOrApplictions);
 
-
+    if(age > 50 && companySize === "small" && whichEnd === "front-end" && fullTimeOrPartTime === "Part-time" && serverSideOrApplictions === "applications"){
+      $("#suggestionOne").show();
+    }
 
     event.preventDefault();
   });
