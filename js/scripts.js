ebfa5acc98e96b8039a3cd2abc3012cbde5b9eb2
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-03-29 12:02:24 -0700

    add the second and third suggestions

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,26 @@ $(document).ready(function(){
     var age = parseInt($("#age").val());
     var companySize = $("#bigOrSmall").val();
     var whichEnd = $("#frontEnd-backEnd").val();
-    var fullTimeOrPartTime = $("input:radio[name=fullTime-PartTime]:checked").val();
+    var fullTimeOrPartTime = $("input:radio[name=FullTime-PartTime]:checked").val();
     var serverSideOrApplictions = $("input:radio[name=which-side]:checked").val();
-    console.log(typeof age);
+    console.log(age);
     console.log(companySize);
     console.log(whichEnd);
     console.log(fullTimeOrPartTime);
     console.log(serverSideOrApplictions);
 
-    if(age > 50 && companySize === "small" && whichEnd === "front-end" && fullTimeOrPartTime === "Part-time" && serverSideOrApplictions === "applications"){
+    if(age > 50 && companySize === "Small" && whichEnd === "Front-end" && fullTimeOrPartTime === "Part-time" && serverSideOrApplictions === "Applications"){
       $("#suggestionOne").show();
+
+    } else if (companySize === "Big" && whichEnd != "Front-end" && serverSideOrApplictions === "Applications"){
+      $("#suggestionTwo").show();
+
+    } else if (age < 80 && companySize === "Big" && serverSideOrApplictions === "Server-side"){
+      $("#suggestionThree").show();
     }
+
+
+
 
     event.preventDefault();
   });
